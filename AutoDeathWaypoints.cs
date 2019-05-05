97c604a9685d63b705338c3794bdb4c6af4f854a
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-05-05 11:19:18 -0400

    Fix Death Waypoints

--- AutoDeathWaypoints.cs
+++ AutoDeathWaypoints.cs
@@ -5,32 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
 
 namespace DeathWaypoints
 {
     class AutoDeathWaypoints : ModSystem
     {
         long id;
+
         public override void StartClientSide(ICoreClientAPI api)
         {
             base.StartClientSide(api);
+            
             id = api.World.RegisterGameTickListener(dt => {
-                if (api.World.Player.Entity != null)
+                EntityPlayer player = api.World.Player.Entity;
+
+                if (player != null)
                 {
-                    api.World.Player.Entity.WatchedAttributes.RegisterModifiedListener("entityDead", () =>
+                    player.WatchedAttributes.RegisterModifiedListener("entityDead", () =>
                     {
-                        if (api.World.Player.Entity.Alive)
+                        if (player.WatchedAttributes["entityDead"].ToString() == "1")
                         {
-                            int color = ColorUtil.HsvToRgb((int)(api.World.Rand.NextDouble() * 255), (int)(api.World.Rand.NextDouble() * 255), (int)(api.World.Rand.NextDouble() * 255));
-                            string hex = color.ToString("X");
-                            api.SendChatMessage("/waypoint add #" + hex + " Player Death Waypoint");
+                            api.SendChatMessage("/waypoint add #" + ColorStuff.RandomHexColor(api) + " Player Death Waypoint");
                         }
                     });
                     api.World.UnregisterGameTickListener(id);
                 }
             }, 500);
-
         }
+    }
+
+    class ColorStuff
+    {
+        public static int RandomColor(ICoreAPI api) => ColorUtil.HsvToRgb(
+            (int)(api.World.Rand.NextDouble() * 255),
+            (int)(api.World.Rand.NextDouble() * 255),
+            (int)(api.World.Rand.NextDouble() * 255)
+            );
+
+        public static string RandomHexColor(ICoreAPI api) => RandomColor(api).ToString("X");
     }
 }
