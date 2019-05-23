3207be58c24acc3e9804ea4f1e45ee2d0704c624
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-05-23 12:52:51 -0400

    tabs as spaces

--- BlockPhysics/BlockPhysicsMod.cs
+++ BlockPhysics/BlockPhysicsMod.cs
@@ -12,160 +12,160 @@ using Vintagestory.GameContent;
 
 namespace StandAloneBlockPhysics
 {
-	public class BlockPhysicsMod : ModSystem
-	{
-		public override void Start(ICoreAPI api)
-		{
-			api.RegisterBlockBehaviorClass("UnstableFalling", typeof(AlteredBlockPhysics));
-		}
-	}
+    public class BlockPhysicsMod : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            api.RegisterBlockBehaviorClass("UnstableFalling", typeof(AlteredBlockPhysics));
+        }
+    }
 
-	public class AlteredBlockPhysics : BlockBehavior
-	{
-		BlockPos[] offset;
-		BlockPos[] cardinal;
-		BlockPos[] supportarea;
+    public class AlteredBlockPhysics : BlockBehavior
+    {
+        BlockPos[] offset;
+        BlockPos[] cardinal;
+        BlockPos[] supportarea;
 
-		public AlteredBlockPhysics(Block block) : base(block)
-		{
-		}
+        public AlteredBlockPhysics(Block block) : base(block)
+        {
+        }
 
-		public override void OnLoaded(ICoreAPI api)
-		{
-			base.OnLoaded(api);
-			offset = AreaMethods.AreaBelowOffsetList().ToArray();
-			cardinal = AreaMethods.CardinalOffsetList().ToArray();
+        public override void OnLoaded(ICoreAPI api)
+        {
+            base.OnLoaded(api);
+            offset = AreaMethods.AreaBelowOffsetList().ToArray();
+            cardinal = AreaMethods.CardinalOffsetList().ToArray();
 
-			supportarea = AreaMethods.LargeAreaBelowOffsetList().ToArray();
-		}
+            supportarea = AreaMethods.LargeAreaBelowOffsetList().ToArray();
+        }
 
-		public override void OnBlockPlaced(IWorldAccessor world, BlockPos pos, ref EnumHandling handled)
-		{
-			TryCollapse(world, pos);
+        public override void OnBlockPlaced(IWorldAccessor world, BlockPos pos, ref EnumHandling handled)
+        {
+            TryCollapse(world, pos);
 
-			base.OnBlockPlaced(world, pos, ref handled);
-		}
+            base.OnBlockPlaced(world, pos, ref handled);
+        }
 
-		public override void OnNeighbourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibpos, ref EnumHandling handling)
-		{
-			TryCollapse(world, pos);
+        public override void OnNeighbourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibpos, ref EnumHandling handling)
+        {
+            TryCollapse(world, pos);
 
-			base.OnNeighbourBlockChange(world, pos, neibpos, ref handling);
-		}
+            base.OnNeighbourBlockChange(world, pos, neibpos, ref handling);
+        }
 
-		public override void OnBlockRemoved(IWorldAccessor world, BlockPos pos, ref EnumHandling handling)
-		{
-			world.BlockAccessor.GetBlock(pos.UpCopy()).OnNeighourBlockChange(world, pos.UpCopy(), pos);
+        public override void OnBlockRemoved(IWorldAccessor world, BlockPos pos, ref EnumHandling handling)
+        {
+            world.BlockAccessor.GetBlock(pos.UpCopy()).OnNeighourBlockChange(world, pos.UpCopy(), pos);
 
-			base.OnBlockRemoved(world, pos, ref handling);
-		}
+            base.OnBlockRemoved(world, pos, ref handling);
+        }
 
-		public void TryCollapse(IWorldAccessor world, BlockPos pos)
-		{
-			if (world.Side.IsClient() || IsSupported(world, pos)) return;
-			world.RegisterCallbackUnique((vworld, vpos, dt) =>
-			{
-				BlockPos dPos = pos.AddCopy(0, -1, 0);
-				Block dBlock = world.BlockAccessor.GetBlock(dPos);
+        public void TryCollapse(IWorldAccessor world, BlockPos pos)
+        {
+            if (world.Side.IsClient() || IsSupported(world, pos)) return;
+            world.RegisterCallbackUnique((vworld, vpos, dt) =>
+            {
+                BlockPos dPos = pos.AddCopy(0, -1, 0);
+                Block dBlock = world.BlockAccessor.GetBlock(dPos);
 
-				if (dBlock.IsReplacableBy(block))
-				{
-					MoveBlock(world, pos, pos.AddCopy(0, -1, 0));
-				}
-				else if (dBlock.CollisionBoxes != null && dBlock.CollisionBoxes[0].Height < 1)
-				{
-					world.BlockAccessor.BreakBlock(pos, null);
-				}
-				else if (dBlock.CollisionBoxes == null || block.CollisionBoxes[0].Length >= dBlock.CollisionBoxes[0].Length)
-				{
-					List<BlockPos> possiblePos = new List<BlockPos>();
-					for (int i = 0; i < offset.Length; i++)
-					{
-						BlockPos offs = pos.AddCopy(offset[i].X, offset[i].Y, offset[i].Z);
-						Block oBlock = world.BulkBlockAccessor.GetBlock(offs);
+                if (dBlock.IsReplacableBy(block))
+                {
+                    MoveBlock(world, pos, pos.AddCopy(0, -1, 0));
+                }
+                else if (dBlock.CollisionBoxes != null && dBlock.CollisionBoxes[0].Height < 1)
+                {
+                    world.BlockAccessor.BreakBlock(pos, null);
+                }
+                else if (dBlock.CollisionBoxes == null || block.CollisionBoxes[0].Length >= dBlock.CollisionBoxes[0].Length)
+                {
+                    List<BlockPos> possiblePos = new List<BlockPos>();
+                    for (int i = 0; i < offset.Length; i++)
+                    {
+                        BlockPos offs = pos.AddCopy(offset[i].X, offset[i].Y, offset[i].Z);
+                        Block oBlock = world.BulkBlockAccessor.GetBlock(offs);
 
-						if (offs.Y < 0 || offs.Y > world.BlockAccessor.MapSizeY) continue;
+                        if (offs.Y < 0 || offs.Y > world.BlockAccessor.MapSizeY) continue;
 
-						if (oBlock.IsReplacableBy(block))
-						{
-							possiblePos.Add(offs);
-						}
-					}
-					if (possiblePos.Count() > 0)
-					{
-						BlockPos toPos = possiblePos[(int)Math.Round((world.Rand.NextDouble() * (possiblePos.Count - 1)))];
+                        if (oBlock.IsReplacableBy(block))
+                        {
+                            possiblePos.Add(offs);
+                        }
+                    }
+                    if (possiblePos.Count() > 0)
+                    {
+                        BlockPos toPos = possiblePos[(int)Math.Round((world.Rand.NextDouble() * (possiblePos.Count - 1)))];
 
-						MoveBlock(world, pos, toPos);
-					}
-				}
-			}, pos, 30);
-		}
+                        MoveBlock(world, pos, toPos);
+                    }
+                }
+            }, pos, 30);
+        }
 
-		public void MoveBlock(IWorldAccessor world, BlockPos fromPos, BlockPos toPos)
-		{
-			if (block.EntityClass != null)
-			{
-				TreeAttribute attribs = new TreeAttribute();
-				BlockEntity be = world.BlockAccessor.GetBlockEntity(fromPos);
-				if (be != null)
-				{
-					be.ToTreeAttributes(attribs);
-					attribs.SetInt("posx", toPos.X);
-					attribs.SetInt("posy", toPos.Y);
-					attribs.SetInt("posz", toPos.Z);
+        public void MoveBlock(IWorldAccessor world, BlockPos fromPos, BlockPos toPos)
+        {
+            if (block.EntityClass != null)
+            {
+                TreeAttribute attribs = new TreeAttribute();
+                BlockEntity be = world.BlockAccessor.GetBlockEntity(fromPos);
+                if (be != null)
+                {
+                    be.ToTreeAttributes(attribs);
+                    attribs.SetInt("posx", toPos.X);
+                    attribs.SetInt("posy", toPos.Y);
+                    attribs.SetInt("posz", toPos.Z);
 
-					world.BulkBlockAccessor.SetBlock(0, fromPos);
-					world.BulkBlockAccessor.SetBlock(block.BlockId, toPos);
-					world.BulkBlockAccessor.Commit();
-					BlockEntity be2 = world.BlockAccessor.GetBlockEntity(toPos);
+                    world.BulkBlockAccessor.SetBlock(0, fromPos);
+                    world.BulkBlockAccessor.SetBlock(block.BlockId, toPos);
+                    world.BulkBlockAccessor.Commit();
+                    BlockEntity be2 = world.BlockAccessor.GetBlockEntity(toPos);
 
-					if (be2 != null) be2.FromTreeAtributes(attribs, world);
-				}
-			}
-			else
-			{
-				world.BulkBlockAccessor.SetBlock(0, fromPos);
-				world.BulkBlockAccessor.SetBlock(block.BlockId, toPos);
-				world.BulkBlockAccessor.Commit();
-			}
-		}
-		
-		public bool IsSupported(IWorldAccessor world, BlockPos pos)
-		{
-			for (int i = 0; i < supportarea.Length; i++)
-			{
-				BlockPos iPos = pos.AddCopy(supportarea[i].X, supportarea[i].Y, supportarea[i].Z);
-				Block iBlock = world.BlockAccessor.GetBlock(iPos);
-				if (iBlock.HasBehavior<BehaviorSupportBeam>())
-				{
-					BlockPos dPos = pos.DownCopy();
-					Block dBlock = world.BlockAccessor.GetBlock(dPos);
-					if (!dBlock.IsReplacableBy(block))
-					{
-						return true;
-					}
-					else
-					{
-						for (int j = 0; j < cardinal.Length; j++)
-						{
-							BlockPos jPos = pos.AddCopy(cardinal[j].X, cardinal[j].Y, cardinal[j].Z);
-							Block jBlock = world.BlockAccessor.GetBlock(jPos);
-							if (!jBlock.IsReplacableBy(block))
-							{
-								return true;
-							}
-						}
-					}
-				}
-			}
-			return false;
-		}
-	}
+                    if (be2 != null) be2.FromTreeAtributes(attribs, world);
+                }
+            }
+            else
+            {
+                world.BulkBlockAccessor.SetBlock(0, fromPos);
+                world.BulkBlockAccessor.SetBlock(block.BlockId, toPos);
+                world.BulkBlockAccessor.Commit();
+            }
+        }
 
-	public class BehaviorSupportBeam : BlockBehavior
-	{
-		public BehaviorSupportBeam(Block block) : base(block)
-		{
-		}
-	}
+        public bool IsSupported(IWorldAccessor world, BlockPos pos)
+        {
+            for (int i = 0; i < supportarea.Length; i++)
+            {
+                BlockPos iPos = pos.AddCopy(supportarea[i].X, supportarea[i].Y, supportarea[i].Z);
+                Block iBlock = world.BlockAccessor.GetBlock(iPos);
+                if (iBlock.HasBehavior<BehaviorSupportBeam>())
+                {
+                    BlockPos dPos = pos.DownCopy();
+                    Block dBlock = world.BlockAccessor.GetBlock(dPos);
+                    if (!dBlock.IsReplacableBy(block))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < cardinal.Length; j++)
+                        {
+                            BlockPos jPos = pos.AddCopy(cardinal[j].X, cardinal[j].Y, cardinal[j].Z);
+                            Block jBlock = world.BlockAccessor.GetBlock(jPos);
+                            if (!jBlock.IsReplacableBy(block))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public class BehaviorSupportBeam : BlockBehavior
+    {
+        public BehaviorSupportBeam(Block block) : base(block)
+        {
+        }
+    }
 }
