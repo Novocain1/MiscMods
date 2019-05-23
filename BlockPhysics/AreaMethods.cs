aa7c8a651f75c0b2625dc969583640072f9d32ec
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-05-23 03:56:48 -0400

    include stand alone block physics

---
+++ BlockPhysics/AreaMethods.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace StandAloneBlockPhysics
+{
+    class AreaMethods
+    {
+        public static BlockPos[] FullAreaAround(BlockPos pos)
+        {
+            BlockPos[] positions = AreaBelow(pos).Concat(AreaAbove(pos)).ToArray();
+            return positions.Concat(AreaAround(pos)).ToArray();
+        }
+
+        public static BlockPos[] AreaAround(BlockPos pos)
+        {
+            return new BlockPos[]
+            {
+               pos,
+               pos.NorthCopy(),
+               pos.SouthCopy(),
+               pos.EastCopy(),
+               pos.WestCopy(),
+               pos.NorthCopy().EastCopy(),
+               pos.SouthCopy().WestCopy(),
+               pos.EastCopy().SouthCopy(),
+               pos.WestCopy().NorthCopy(),
+            };
+        }
+
+        public static BlockPos[] AreaBelow(BlockPos pos)
+        {
+            BlockPos aPos = pos.DownCopy();
+            return new BlockPos[]
+            {
+               aPos,
+               aPos.NorthCopy(),
+               aPos.SouthCopy(),
+               aPos.EastCopy(),
+               aPos.WestCopy(),
+               aPos.NorthCopy().EastCopy(),
+               aPos.SouthCopy().WestCopy(),
+               aPos.EastCopy().SouthCopy(),
+               aPos.WestCopy().NorthCopy(),
+            };
+        }
+
+        public static List<BlockPos> AreaBelowOffsetList()
+        {
+            List<BlockPos> positions = new List<BlockPos>();
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int z = -1; z <= 1; z++)
+                {
+                    positions.Add(new BlockPos(x, -1, z));
+                }
+            }
+            return positions;
+        }
+
+        public static List<BlockPos> AreaBelowCardinalOffsetList()
+        {
+            List<BlockPos> positions = new List<BlockPos> {
+                new BlockPos(-1,-1,0),
+                new BlockPos(1,-1,0),
+                new BlockPos(0,-1,-1),
+                new BlockPos(0,-1,1),
+            };
+            return positions;
+        }
+
+        public static BlockPos[] LargeAreaBelow(BlockPos pos)
+        {
+            List<BlockPos> positions = new List<BlockPos>();
+            for (int x = -3; x <= 3; x++)
+            {
+                for (int z = -3; z <= 3; z++)
+                {
+                    positions.Add(pos.AddCopy(x, -1, z));
+                }
+            }
+            return positions.ToArray();
+        }
+
+        public static List<BlockPos> LargeAreaBelowOffsetList()
+        {
+            List<BlockPos> positions = new List<BlockPos>();
+            for (int x = -3; x <= 3; x++)
+            {
+                for (int z = -3; z <= 3; z++)
+                {
+                    positions.Add(new BlockPos(x, -1, z));
+                }
+            }
+            return positions;
+        }
+
+        public static List<BlockPos> AreaAroundOffsetList()
+        {
+            List<BlockPos> positions = new List<BlockPos>();
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int z = -1; z <= 1; z++)
+                    {
+                        positions.Add(new BlockPos(x, y, z));
+                    }
+                }
+            }
+            return positions;
+        }
+
+        public static List<BlockPos> SphericalOffsetList(int radius)
+        {
+            List<BlockPos> positions = new List<BlockPos>();
+            for (int x = -radius; x <= radius; x++)
+            {
+                for (int y = -radius; y <= radius; y++)
+                {
+                    for (int z = -radius; z <= radius; z++)
+                    {
+                        if ((x*x+y*y+z*z) <= (radius*radius))
+                        {
+                            positions.Add(new BlockPos(x, y, z));
+                        }
+                    }
+                }
+            }
+            return positions;
+        }
+
+        public static List<BlockPos> CircularOffsetList(int radius)
+        {
+            List<BlockPos> positions = new List<BlockPos>();
+            for (int x = -radius; x <= radius; x++)
+            {
+                for (int z = -radius; z <= radius; z++)
+                {
+                    if ((x * x + z * z) <= (radius * radius))
+                    {
+                        positions.Add(new BlockPos(x, 0, z));
+                    }
+                }
+
+            }
+            return positions;
+        }
+
+        public static List<BlockPos> PlanarAreaAroundOffsetList()
+        {
+            List<BlockPos> positions = new List<BlockPos>();
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int z = -1; z <= 1; z++)
+                {
+                    positions.Add(new BlockPos(x, 0, z));
+                }
+
+            }
+            return positions;
+        }
+
+        public static List<BlockPos> CardinalOffsetList()
+        {
+            return new List<BlockPos>
+            {
+                new BlockPos(0,0,1),
+                new BlockPos(1,0,0),
+                new BlockPos(0,0,-1),
+                new BlockPos(-1,0,0),
+            };
+        }
+
+        public static BlockPos[] AreaAbove(BlockPos pos)
+        {
+            BlockPos aPos = pos.UpCopy();
+            return new BlockPos[]
+            {
+               aPos,
+               aPos.NorthCopy(),
+               aPos.SouthCopy(),
+               aPos.EastCopy(),
+               aPos.WestCopy(),
+               aPos.NorthCopy().EastCopy(),
+               aPos.SouthCopy().WestCopy(),
+               aPos.EastCopy().SouthCopy(),
+               aPos.WestCopy().NorthCopy(),
+            };
+        }
+
+        public static BlockPos[] Cardinal(BlockPos pos)
+        {
+            return new BlockPos[]
+            {
+               pos.NorthCopy(),
+               pos.SouthCopy(),
+               pos.EastCopy(),
+               pos.WestCopy(),
+            };
+        }
+
+
+        public static BlockPos[] FullCardinal(BlockPos pos)
+        {
+            return new BlockPos[]
+            {
+               pos.UpCopy(),
+               pos.DownCopy(),
+               pos.NorthCopy(),
+               pos.SouthCopy(),
+               pos.EastCopy(),
+               pos.WestCopy(),
+            };
+        }
+
+        public static Block[] BlockCardinal(IWorldAccessor world, BlockPos pos)
+        {
+            List<Block> blocks = new List<Block>();
+            foreach (var val in Cardinal(pos))
+            {
+                blocks.Add(world.BulkBlockAccessor.GetBlock(val));
+            }
+            return blocks.ToArray();
+        }
+
+        public static Block[] BlockFullCardinal(IWorldAccessor world, BlockPos pos)
+        {
+            List<Block> blocks = new List<Block>();
+            foreach (var val in FullCardinal(pos))
+            {
+                blocks.Add(world.BulkBlockAccessor.GetBlock(val));
+            }
+            return blocks.ToArray();
+        }
+
+        public static int CardinalCount(IWorldAccessor world, BlockPos pos)
+        {
+            List<Block> blocks = new List<Block>();
+            int count = 0;
+            Block block = world.BulkBlockAccessor.GetBlock(pos);
+            foreach (var val in BlockCardinal(world, pos))
+            {
+                if (!val.IsReplacableBy(block))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static Dictionary<BlockPos, string> CardinalDict(BlockPos pos)
+        {
+            return new Dictionary<BlockPos, string>
+            {
+                {   pos.NorthCopy(), "north" },
+                {   pos.SouthCopy(), "south" },
+                {   pos.EastCopy(), "east" },
+                {   pos.WestCopy(), "west" }
+            };
+        }
+
+        public static Dictionary<string, BlockPos> DirectionDict(BlockPos pos)
+        {
+            return new Dictionary<string, BlockPos>
+            {
+                {   "north", pos.NorthCopy() },
+                {   "south", pos.SouthCopy() },
+                {   "east", pos.EastCopy() },
+                {   "west", pos.WestCopy() }
+            };
+        }
+
+        public static Dictionary<int, string> CountString()
+        {
+            return new Dictionary<int, string> {
+                { 0, "zero" },
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" },
+                { 4, "four" },
+                { 5, "five" },
+                { 6, "six" },
+                { 7, "seven" },
+                { 8, "eight" },
+                { 9, "nine" },
+            };
+        }
+
+        public static Dictionary<string, string> CornerMatches()
+        {
+            return new Dictionary<string, string>
+            {
+                {"north","east" },
+                {"south","west" },
+            };
+        }
+	}
+}
---
+++ BlockPhysics/BlockPhysicsMod.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Input;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+
+namespace StandAloneBlockPhysics
+{
+	public class BlockPhysicsMod : ModSystem
+	{
+		public override void Start(ICoreAPI api)
+		{
+			api.RegisterBlockBehaviorClass("UnstableFalling", typeof(AlteredBlockPhysics));
+		}
+	}
+
+	public class AlteredBlockPhysics : BlockBehavior
+	{
+		BlockPos[] offset;
+		BlockPos[] cardinal;
+
+		public AlteredBlockPhysics(Block block) : base(block)
+		{
+		}
+
+		public override void OnLoaded(ICoreAPI api)
+		{
+			base.OnLoaded(api);
+			offset = AreaMethods.AreaBelowOffsetList().ToArray();
+			List<BlockPos> blocks = AreaMethods.CardinalOffsetList();
+			blocks.Add(new BlockPos(0, 1, 0));
+			blocks.Add(new BlockPos(0, -1, 0));
+			cardinal = blocks.ToArray();
+		}
+
+		public override void OnBlockPlaced(IWorldAccessor world, BlockPos pos, ref EnumHandling handled)
+		{
+			TryCollapse(world, pos);
+			base.OnBlockPlaced(world, pos, ref handled);
+		}
+
+		public override void OnNeighbourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibpos, ref EnumHandling handling)
+		{
+			TryCollapse(world, pos);
+			base.OnNeighbourBlockChange(world, pos, neibpos, ref handling);
+		}
+
+		public override void OnBlockRemoved(IWorldAccessor world, BlockPos pos, ref EnumHandling handling)
+		{
+			world.BlockAccessor.GetBlock(pos.UpCopy()).OnNeighourBlockChange(world, pos.UpCopy(), pos);
+			base.OnBlockRemoved(world, pos, ref handling);
+		}
+
+		public void TryCollapse(IWorldAccessor world, BlockPos pos)
+		{
+			if (world.Side.IsClient()) return;
+			world.RegisterCallbackUnique((vworld, vpos, dt) =>
+			{
+				BlockPos dPos = pos.AddCopy(0, -1, 0);
+				Block dBlock = world.BlockAccessor.GetBlock(dPos);
+
+				if (dBlock.IsReplacableBy(block))
+				{
+					MoveBlock(world, pos, pos.AddCopy(0, -1, 0));
+				}
+				else if (dBlock.CollisionBoxes != null && dBlock.CollisionBoxes[0].Height < 1)
+				{
+					world.BlockAccessor.BreakBlock(pos, null);
+				}
+				else if (dBlock.CollisionBoxes == null || block.CollisionBoxes[0].Length >= dBlock.CollisionBoxes[0].Length)
+				{
+					List<BlockPos> possiblePos = new List<BlockPos>();
+					for (int i = 0; i < offset.Length; i++)
+					{
+						BlockPos offs = pos.AddCopy(offset[i].X, offset[i].Y, offset[i].Z);
+						if (offs.Y < 0 || offs.Y > world.BlockAccessor.MapSizeY) continue;
+
+						if (world.BulkBlockAccessor.GetBlock(offs).IsReplacableBy(block))
+						{
+							possiblePos.Add(offs);
+						}
+					}
+					if (possiblePos.Count() > 0)
+					{
+						BlockPos toPos = possiblePos[(int)Math.Round((world.Rand.NextDouble() * (possiblePos.Count - 1)))];
+
+						MoveBlock(world, pos, toPos);
+					}
+				}
+			}, pos, 30);
+		}
+
+		public void MoveBlock(IWorldAccessor world, BlockPos fromPos, BlockPos toPos)
+		{
+			if (block.EntityClass != null)
+			{
+				TreeAttribute attribs = new TreeAttribute();
+				BlockEntity be = world.BlockAccessor.GetBlockEntity(fromPos);
+				if (be != null)
+				{
+					be.ToTreeAttributes(attribs);
+					attribs.SetInt("posx", toPos.X);
+					attribs.SetInt("posy", toPos.Y);
+					attribs.SetInt("posz", toPos.Z);
+
+					world.BulkBlockAccessor.SetBlock(0, fromPos);
+					world.BulkBlockAccessor.SetBlock(block.BlockId, toPos);
+					world.BulkBlockAccessor.Commit();
+					BlockEntity be2 = world.BlockAccessor.GetBlockEntity(toPos);
+
+					if (be2 != null) be2.FromTreeAtributes(attribs, world);
+				}
+			}
+			else
+			{
+				world.BulkBlockAccessor.SetBlock(0, fromPos);
+				world.BulkBlockAccessor.SetBlock(block.BlockId, toPos);
+				world.BulkBlockAccessor.Commit();
+			}
+		}
+	}
+
+}
