a2f58987e4fcdd64ab6f7308ccc3a20b4713977f
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-08-03 16:03:35 -0400

    Update Clock GUI

--- Waypoint Utils/Utility/Clock.cs
+++ Waypoint Utils/Utility/Clock.cs
@@ -12,11 +12,27 @@ namespace WaypointUtils
 {
     class ClockDialogSystem : ModSystem
     {
+        private long id;
+
         public override void StartClientSide(ICoreClientAPI api)
         {
             GuiDialogClock clock = new GuiDialogClock(api);
 
             api.Input.RegisterHotKey("clock", "Open Clock GUI", GlKeys.ControlRight, HotkeyType.GUIOrOtherControls);
+
+            id = api.Event.RegisterGameTickListener(dt => 
+            {
+                if (api.World.Player?.Entity != null)
+                {
+                    if (api.Settings.Bool["clockGui"])
+                    {
+                        clock.TryOpen();
+                    }
+
+                    api.Event.UnregisterGameTickListener(id);
+                }
+                
+            }, 1000);
 
             api.Input.SetHotKeyHandler("clock", a =>
             {
@@ -29,6 +45,7 @@ namespace WaypointUtils
     class GuiDialogClock : HudElement
     {
         long id;
+        long id2;
 
         public GuiDialogClock(ICoreClientAPI capi) : base(capi)
         {
@@ -38,54 +55,46 @@ namespace WaypointUtils
         public override void OnOwnPlayerDataReceived()
         {
             base.OnOwnPlayerDataReceived();
-            GameCalendar cal = (GameCalendar)capi.World.Calendar;
 
             ElementBounds textBounds = ElementBounds.Fixed(EnumDialogArea.LeftMiddle, 5, -250, 500, 500);
-            BlockPos entityPos = capi.World.Player.Entity.Pos.AsBlockPos;
-            ClimateCondition climate = capi.World.BlockAccessor.GetClimateAt(entityPos);
-
-            string hour = cal.FullHourOfDay < 10 ? "0" + cal.FullHourOfDay : "" + cal.FullHourOfDay;
-            int m = (int)(60 * (cal.HourOfDay - cal.FullHourOfDay));
-            string minute = m < 10 ? "0" + m : "" + m;
-            StringBuilder stringBuilder = new StringBuilder();
-            
-            stringBuilder.AppendLine("Date: " + cal.DayOfYear + "/" + cal.DaysPerYear + ", " + cal.Year)
-                .AppendLine("Time: " + hour + ":" + minute)
-                .AppendLine("Global Season: " + cal.Season)
-                .AppendLine("Local Temperature: " + Math.Round(climate.Temperature, 3))
-                .AppendLine("Local Rainfall: " + Math.Round(climate.Rainfall, 3));
 
             double[] stroke = new double[] { 0, 0, 0, 1 };
 
             SingleComposer = capi.Gui.CreateCompo("clock", textBounds)
-                .AddDynamicText(stringBuilder.ToString(), CairoFont.WhiteSmallText().WithStroke(stroke, 2), EnumTextOrientation.Justify, textBounds, "clock")
+                .AddDynamicText("", CairoFont.WhiteSmallText().WithStroke(stroke, 2), EnumTextOrientation.Justify, textBounds, "clock")
                 .Compose();
 
             bool dot = false;
+            string d = "";
 
-            id = capi.World.RegisterGameTickListener(dt => 
-            {
-                entityPos = capi.World.Player.Entity.Pos.AsBlockPos;
-                climate = capi.World.BlockAccessor.GetClimateAt(entityPos);
+            UpdateText(d);
 
-                cal = (GameCalendar)capi.World.Calendar;
+            id2 = capi.World.RegisterGameTickListener(dt => d = (dot = !dot) ? ":" : " ", 2000);
+            id = capi.World.RegisterGameTickListener(dt => UpdateText(d), 30);
+        }
 
-                dot = !dot;
-                string d = dot ? ":" : " ";
+        public void UpdateText(string dot)
+        {
+            BlockPos entityPos = capi.World.Player.Entity.Pos.AsBlockPos;
+            ClimateCondition climate = capi.World.BlockAccessor.GetClimateAt(entityPos);
 
-                hour = cal.FullHourOfDay < 10 ? "0" + cal.FullHourOfDay : "" + cal.FullHourOfDay;
-                m = (int)(60 * (cal.HourOfDay - cal.FullHourOfDay));
-                minute = m < 10 ? "0" + m : "" + m;
+            GameCalendar cal = (GameCalendar)capi.World.Calendar;
 
-                stringBuilder = new StringBuilder();
-                stringBuilder.AppendLine("Date: " + cal.DayOfYear + "/" + cal.DaysPerYear + ", " + cal.Year)
-                    .AppendLine("Time: " + hour + ":" + minute)
-                    .AppendLine("Global Season: " + cal.Season)
-                    .AppendLine("Local Temperature: " + Math.Round(climate.Temperature, 3))
-                    .AppendLine("Local Rainfall: " + Math.Round(climate.Rainfall, 3));
+            string hour = cal.FullHourOfDay < 10 ? "0" + cal.FullHourOfDay : "" + cal.FullHourOfDay;
+            int m = (int)(60 * (cal.HourOfDay - cal.FullHourOfDay));
+            string minute = m < 10 ? "0" + m : "" + m;
 
-                SingleComposer.GetDynamicText("clock").SetNewText(stringBuilder.ToString());
-            }, 1000);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Date: " + cal.DayOfYear + "/" + cal.DaysPerYear + ", " + cal.Year)
+                .AppendLine("Time: " + hour + dot + minute)
+                .AppendLine("Global Season: " + cal.Season)
+                .AppendLine("Average Temperature: " + Math.Round(climate.Temperature, 3))
+                .AppendLine("Average Rainfall: " + Math.Round(climate.Rainfall, 3))
+                .AppendLine("Average Fertility: " + Math.Round(climate.Fertility, 3))
+                .AppendLine("Forest Density: " + Math.Round(climate.ForestDensity, 3))
+                .AppendLine("Shrub Density: " + Math.Round(climate.ShrubDensity, 3));
+
+            SingleComposer.GetDynamicText("clock").SetNewText(stringBuilder.ToString());
         }
 
         public override bool TryOpen()
@@ -93,6 +102,7 @@ namespace WaypointUtils
             if (base.TryOpen())
             {
                 OnOwnPlayerDataReceived();
+                capi.Settings.Bool["clockGui"] = true;
                 return true;
             }
             return false;
@@ -102,6 +112,7 @@ namespace WaypointUtils
         {
             if (base.TryClose())
             {
+                capi.Settings.Bool["clockGui"] = false;
                 Dispose();
                 return true;
             }
@@ -112,6 +123,7 @@ namespace WaypointUtils
         {
             base.Dispose();
             capi.World.UnregisterGameTickListener(id);
+            capi.World.UnregisterGameTickListener(id2);
         }
 
         public override string ToggleKeyCombinationCode => "clock";
