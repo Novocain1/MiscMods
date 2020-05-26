6881be8b1f968cd527416af4fc8323018b19623b
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2020-05-26 09:03:17 -0400

    raw food, seeds, and crops

---
+++ HarvestCraftLoader/HackerMan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace HarvestCraftLoader
+{
+    public static class HackerMan
+    {
+        public static BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+
+        public static K GetField<T, K>(this T instance, string fieldName)
+        {
+            return (K)instance.GetField(fieldName);
+        }
+
+        public static object GetField<T>(this T instance, string fieldName)
+        {
+            return GetInstanceField(instance.GetType(), instance, fieldName);
+        }
+
+        public static object CallMethod<T>(this T instance, string methodName) => instance?.CallMethod(methodName, null);
+
+        public static object CallMethod<T>(this T instance, string methodName, params object[] parameters)
+        {
+            MethodInfo info = instance?.GetType()?.GetMethod(methodName, bindFlags);
+            return info?.Invoke(instance, parameters);
+        }
+
+        public static object GetInstanceField(Type type, object instance, string fieldName)
+        {
+            FieldInfo field = type.GetField(fieldName, bindFlags);
+            return field.GetValue(instance);
+        }
+    }
+}
--- HarvestCraftLoader/HarvestCraftLoader.cs
+++ HarvestCraftLoader/HarvestCraftLoader.cs
@@ -20,14 +20,16 @@ using Path = System.IO.Path;
 using System.Globalization;
 using Vintagestory.API.Server;
 
-namespace VSMod
+namespace HarvestCraftLoader
 {
-    class HarvestCraftLoader : ModSystem
+    class HarvestCraftLoaderMod : ModSystem
     {
         public override void Start(ICoreAPI api)
         {
             api.RegisterBlockClass("BlockCake", typeof(BlockCake));
+            api.RegisterBlockClass("BlockCropHC", typeof(BlockCropHC));
             api.RegisterBlockEntityClass("Cake", typeof(BlockEntityCake));
+            api.RegisterItemClass("ItemPlantableSeedHC", typeof(ItemPlantableSeedHC));
         }
     }
 
@@ -85,6 +87,7 @@ namespace VSMod
     {
         uneaten, slice1, slice2, slice3, slice4, slice5, slice6
     }
+
     class MultiPropFood
     {
         [JsonProperty]
@@ -102,6 +105,70 @@ namespace VSMod
                     player?.Entity.ReceiveSaturation(NutritionProperties[j].Satiety / Division, NutritionProperties[j].FoodCategory);
                 }
             }
+        }
+    }
+
+    class ItemPlantableSeedHC : ItemPlantableSeed
+    {
+        public override void OnHeldInteractStart(ItemSlot itemslot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling)
+        {
+            if (blockSel == null) return;
+
+            BlockPos pos = blockSel.Position;
+
+            string lastCodePart = itemslot.Itemstack.Collectible.LastCodePart();
+
+            BlockEntity be = byEntity.World.BlockAccessor.GetBlockEntity(pos);
+            if (be is BlockEntityFarmland)
+            {
+                Block cropBlock = byEntity.World.GetBlock(CodeWithPath("crops-" + lastCodePart + "-0"));
+                if (cropBlock == null) return;
+
+                IPlayer byPlayer = null;
+                if (byEntity is EntityPlayer) byPlayer = byEntity.World.PlayerByUid(((EntityPlayer)byEntity).PlayerUID);
+
+                bool planted = (bool)((BlockEntityFarmland)be).CallMethod("TryPlant", cropBlock);
+                if (planted)
+                {
+                    byEntity.World.PlaySoundAt(new AssetLocation("sounds/block/plant"), pos.X, pos.Y, pos.Z, byPlayer);
+
+                    ((byEntity as EntityPlayer)?.Player as IClientPlayer)?.TriggerFpAnimation(EnumHandInteract.HeldItemInteract);
+
+                    if (byPlayer?.WorldData?.CurrentGameMode != EnumGameMode.Creative)
+                    {
+                        itemslot.TakeOut(1);
+                        itemslot.MarkDirty();
+                    }
+                }
+
+                if (planted) handHandling = EnumHandHandling.PreventDefault;
+            }
+        }
+
+        public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
+        {
+            base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
+
+            Block cropBlock = world.GetBlock(CodeWithPath("crops-" + inSlot.Itemstack.Collectible.LastCodePart() + "-1"));
+            if (cropBlock == null || cropBlock.CropProps == null) return;
+
+            dsc.AppendLine(Lang.Get("soil-nutrition-requirement") + cropBlock.CropProps.RequiredNutrient);
+            dsc.AppendLine(Lang.Get("soil-nutrition-consumption") + cropBlock.CropProps.NutrientConsumption);
+            dsc.AppendLine(Lang.Get("soil-growth-time") + Math.Round(cropBlock.CropProps.TotalGrowthDays, 1) + " days");
+        }
+    }
+    
+    class BlockCropHC : BlockCrop
+    {
+        public override string GetPlacedBlockInfo(IWorldAccessor world, BlockPos pos, IPlayer forPlayer)
+        {
+            string info = world.BlockAccessor.GetBlock(pos.DownCopy()).GetPlacedBlockInfo(world, pos.DownCopy(), forPlayer);
+
+            return
+                Lang.Get("Required Nutrient: {0}", CropProps.RequiredNutrient) + "\n" +
+                Lang.Get("Growth Stage: {0} / {1}", CurrentStage(), CropProps.GrowthStages - 1) +
+                (info != null && info.Length > 0 ? "\n\n" + Lang.Get("soil-tooltip") + "\n" + info : "")
+            ;
         }
     }
 }
