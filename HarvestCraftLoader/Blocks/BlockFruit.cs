0c790164939232418c3ad6152eb90f2c62e3fff2
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2020-06-02 20:32:17 -0400

    initial world generation of fruits

---
+++ HarvestCraftLoader/Blocks/BlockFruit.cs
@@ -0,0 +1,62 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace HarvestCraftLoader
+{
+    class BlockFruit : BlockPlantFix
+    {
+        public override bool CanPlantStay(IBlockAccessor blockAccessor, BlockPos pos)
+        {
+            Block block = blockAccessor.GetBlock(pos.X, pos.Y + 1, pos.Z);
+            return block.BlockMaterial == EnumBlockMaterial.Leaves;
+        }
+
+        public override bool TryPlaceBlock(IWorldAccessor world, IPlayer byPlayer, ItemStack itemstack, BlockSelection blockSel, ref string failureCode)
+        {
+            bool test = base.TryPlaceBlock(world, byPlayer, itemstack, blockSel, ref failureCode);
+            failureCode = test ? "__ignore__" : "requiresleavesabove";
+            return test;
+        }
+
+        //mostly copied from BlockBeehive
+        public override bool TryPlaceBlockForWorldGen(IBlockAccessor blockAccessor, BlockPos pos, BlockFacing onBlockFace, LCGRandom worldgenRand)
+        {
+            Block cinnamon = blockAccessor.GetBlock(new AssetLocation("harvestcraftloader:fruits-cinnamon-ripe"));
+
+            for (int i = 1; i < 4; i++)
+            {
+                Block aboveBlock = blockAccessor.GetBlock(pos.X, pos.Y - i, pos.Z);
+
+                if ((aboveBlock.BlockMaterial == EnumBlockMaterial.Wood || aboveBlock.BlockMaterial == EnumBlockMaterial.Leaves) && aboveBlock.SideSolid[BlockFacing.DOWN.Index])
+                {
+                    BlockPos atpos = new BlockPos(pos.X, pos.Y - i - 1, pos.Z);
+
+                    Block block = blockAccessor.GetBlock(atpos);
+
+                    if (block.BlockMaterial == EnumBlockMaterial.Wood && aboveBlock.BlockMaterial == EnumBlockMaterial.Wood && blockAccessor.GetBlock(pos.X, pos.Y - i - 2, pos.Z).BlockMaterial == EnumBlockMaterial.Wood && aboveBlock.LastCodePart() == "ud")
+                    {
+                        blockAccessor.SetBlock(cinnamon.BlockId, atpos);
+                        if (EntityClass != null) blockAccessor.SpawnBlockEntity(EntityClass, atpos);
+
+                        return true;
+                    }
+
+                    if (aboveBlock.BlockMaterial == EnumBlockMaterial.Wood || Id == cinnamon.Id || (block.BlockMaterial != EnumBlockMaterial.Leaves && block.BlockMaterial != EnumBlockMaterial.Air)) continue;
+
+                    int dx = pos.X % blockAccessor.ChunkSize;
+                    int dz = pos.Z % blockAccessor.ChunkSize;
+                    int surfacey = blockAccessor.GetMapChunkAtBlockPos(atpos).WorldGenTerrainHeightMap[dz * blockAccessor.ChunkSize + dx];
+
+                    if (pos.Y - surfacey < 4) return false;
+
+                    blockAccessor.SetBlock(BlockId, atpos);
+                    if (EntityClass != null) blockAccessor.SpawnBlockEntity(EntityClass, atpos);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ HarvestCraftLoader/Blocks/BlockPlantFix.cs
@@ -0,0 +1,91 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Client;
+using Vintagestory.API.MathTools;
+
+namespace HarvestCraftLoader
+{
+    public class BlockPlantFix : Block
+    {
+        public override void OnJsonTesselation(ref MeshData sourceMesh, BlockPos pos, int[] chunkExtIds, ushort[] chunkLightExt, int extIndex3d)
+        {
+            int sunLightLevel = chunkLightExt[extIndex3d] & 31;
+            bool waveOff = sunLightLevel < 14;
+
+            if (VertexFlags.GrassWindWave)
+            {
+                setLeaveWaveFlags(sourceMesh, waveOff);
+            }
+        }
+
+
+        void setLeaveWaveFlags(MeshData sourceMesh, bool off)
+        {
+            int grassWave = VertexFlags.FoliageWindWaveBitMask;
+            int clearFlags = (~VertexFlags.FoliageWindWaveBitMask) & (~VertexFlags.GroundDistanceBitMask);
+
+            // Iterate over each element face
+            for (int vertexNum = 0; vertexNum < sourceMesh.GetVerticesCount(); vertexNum++)
+            {
+                float y = sourceMesh.xyz[vertexNum * 3 + 1];
+
+                bool notwaving = off || y < 0.5;
+
+                sourceMesh.Flags[vertexNum] &= clearFlags;
+
+                if (!notwaving)
+                {
+                    sourceMesh.Flags[vertexNum] |= grassWave;
+                }
+            }
+        }
+
+
+        public override bool TryPlaceBlock(IWorldAccessor world, IPlayer byPlayer, ItemStack itemstack, BlockSelection blockSel, ref string failureCode)
+        {
+            if (CanPlantStay(world.BlockAccessor, blockSel.Position))
+            {
+                return base.TryPlaceBlock(world, byPlayer, itemstack, blockSel, ref failureCode);
+            }
+
+            failureCode = "requirefertileground";
+
+            return false;
+        }
+
+
+        public override void OnNeighourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibpos)
+        {
+            if (!CanPlantStay(world.BlockAccessor, pos))
+            {
+                world.BlockAccessor.BreakBlock(pos, null);
+                world.BlockAccessor.TriggerNeighbourBlockUpdate(pos);
+            }
+        }
+
+        public virtual bool CanPlantStay(IBlockAccessor blockAccessor, BlockPos pos)
+        {
+            Block block = blockAccessor.GetBlock(pos.X, pos.Y - 1, pos.Z);
+            return block.Fertility > 0;
+        }
+
+
+        public override bool TryPlaceBlockForWorldGen(IBlockAccessor blockAccessor, BlockPos pos, BlockFacing onBlockFace, LCGRandom worldGenRand)
+        {
+            if (!CanPlantStay(blockAccessor, pos)) return false;
+            return base.TryPlaceBlockForWorldGen(blockAccessor, pos, onBlockFace, worldGenRand);
+        }
+
+        public override int GetRandomColor(ICoreClientAPI capi, BlockPos pos, BlockFacing facing)
+        {
+            int color = base.GetRandomColor(capi, pos, facing);
+
+            if (EntityClass == "Sapling")
+            {
+                color = capi.ApplyColorTintOnRgba(1, color, pos.X, pos.Y, pos.Z);
+            }
+
+            return color;
+        }
+
+    }
+}
