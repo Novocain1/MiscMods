886fcbb847691673d95d246d5baa6fca65908787
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2020-11-23 01:01:04 -0500

    rename reshadeenabler to customglsetter

--- CustomGLSetter/CustomGLSetter.cs
+++ CustomGLSetter/CustomGLSetter.cs
@@ -1,31 +1,24 @@
-﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using Mono.Cecil;
 using Vintagestory.API.Config;
 using System.IO;
-using Vintagestory.API.Common;
-using Vintagestory.API.Client;
-using Vintagestory;
 using System.Diagnostics;
 
-namespace ReshadeEnablerMod
+namespace CustomGLSetter
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            ReshadeEnabler enabler = new ReshadeEnabler();
+            CustomGL enabler = new CustomGL();
             enabler.Patch();
             enabler.StartVS(args);
         }
     }
 
-    public class ReshadeEnabler
+    public class CustomGL
     {
         public AssemblyDefinition OpenTKRef { get; set; }
         string LibFolder { get; set; }
@@ -34,7 +27,7 @@ namespace ReshadeEnablerMod
         string OpenTKBackupPath { get; set; }
         string GLPath { get; set; }
 
-        public ReshadeEnabler()
+        public CustomGL()
         {
             LibFolder = Path.Combine(GamePaths.Binaries, "lib");
             OpenTKPath = Path.Combine(LibFolder, "OpenTK.dll");
@@ -74,28 +67,5 @@ namespace ReshadeEnablerMod
             Process.Start("VintageStory.exe", bdr.ToString());
             Environment.Exit(0);
         }
-    }
-
-    public static class HackMan
-    {
-        public static T GetField<T>(this object instance, string fieldname) => (T)AccessTools.Field(instance.GetType(), fieldname).GetValue(instance);
-        public static T GetProperty<T>(this object instance, string fieldname) => (T)AccessTools.Property(instance.GetType(), fieldname).GetValue(instance);
-        public static T CallMethod<T>(this object instance, string method, params object[] args) => (T)AccessTools.Method(instance.GetType(), method).Invoke(instance, args);
-        public static void CallMethod(this object instance, string method, params object[] args) => AccessTools.Method(instance.GetType(), method)?.Invoke(instance, args);
-        public static void CallMethod(this object instance, string method) => AccessTools.Method(instance.GetType(), method)?.Invoke(instance, null);
-        public static object CreateInstance(this Type type) => AccessTools.CreateInstance(type);
-        public static T[] GetFields<T>(this object instance)
-        {
-            List<T> fields = new List<T>();
-            var declaredFields = AccessTools.GetDeclaredFields(instance.GetType())?.Where((t) => t.FieldType == typeof(T));
-            foreach (var val in declaredFields)
-            {
-                fields.Add(instance.GetField<T>(val.Name));
-            }
-            return fields.ToArray();
-        }
-
-        public static void SetField(this object instance, string fieldname, object setVal) => AccessTools.Field(instance.GetType(), fieldname).SetValue(instance, setVal);
-        public static MethodInfo GetMethod(this object instance, string method) => AccessTools.Method(instance.GetType(), method);
     }
 }
