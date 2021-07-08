7f7d019de3d2a95904157229c5f44ad356be3d4a
Novocain <22160223+Novocain1@users.noreply.github.com>
2021-07-07 22:28:22 -0400

    move type

---
+++ VSHUD/Systems/Patches/MarkDirtyFix.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Vintagestory.GameContent;
+
+namespace VSHUD
+{
+    [HarmonyPatch(typeof(GuiDialogEditWayPoint), "onSave")]
+    class MarkDirtyFix
+    {
+        public static void Postfix(GuiDialogEditWayPoint __instance)
+        {
+            int wp = __instance.GetField<int>("wpIndex");
+            lock (FloatyWaypointManagement.WaypointElements)
+            {
+                foreach(var val in FloatyWaypointManagement.WaypointElements)
+                {
+                    if (wp == val.waypointID)
+                    {
+                        val.MarkDirty();
+                    }
+                }
+            }
+        }
+    }
+}
