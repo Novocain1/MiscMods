a1e023b0c901ba01e0d53a0fe70ace2e9ac30df8
Novocain <22160223+Novocain1@users.noreply.github.com>
2021-07-26 01:30:00 -0400

    entities

--- VSHUD/Systems/AutoWaypoints.cs
+++ VSHUD/Systems/AutoWaypoints.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
 using Vintagestory.API.MathTools;
 using Vintagestory.Client.NoObf;
@@ -21,6 +22,11 @@ namespace VSHUD
 
         public Dictionary<AssetLocation, WaypointClassification> WaypointClassifications = new Dictionary<AssetLocation, WaypointClassification>();
 
+        public AssetLocation GetClassifiedAssetLocation(AssetLocation loc, string category)
+        {
+            return new AssetLocation(string.Format("{0}:{1}-{2}", loc.Domain, category, loc.ToShortString()));
+        }
+
         public override void StartClientSide(ICoreClientAPI api)
         {
             capi = api;
@@ -28,17 +34,28 @@ namespace VSHUD
             WaypointClassifications.Clear();
             api.Event.LevelFinalize += () =>
             {
-                Lang.Load(api.World.Logger, api.World.AssetManager, ClientSettings.Language);
-
                 foreach (var block in api.World.Blocks)
                 {
                     if (block.Code == null) continue;
                     string col = ColorUtil.Int2Hex(~block.GetColor(api, new BlockPos()));
                     col = col.Insert(1, "FF");
-                    WaypointClassifications[block.Code] = new WaypointClassification()
+                    AssetLocation loc = GetClassifiedAssetLocation(block.Code, "block");
+                    WaypointClassifications[loc] = new WaypointClassification()
                     {
-                        Name = Lang.GetMatching(block.Code.Domain + ":block-" + block.Code.ToShortString()),
+                        Name = Lang.GetMatching(loc.ToString()),
                         Color = col
+                    };
+                }
+
+                foreach (var entity in api.World.EntityTypes)
+                {
+                    if (entity.Code == null) continue;
+
+                    AssetLocation loc = GetClassifiedAssetLocation(entity.Code, "item-creature");
+                    WaypointClassifications[loc] = new WaypointClassification()
+                    {
+                        Name = Lang.GetMatching(loc.ToString()),
+                        Color = "#FFFF0000"
                     };
                 }
             };
@@ -46,9 +63,29 @@ namespace VSHUD
             api.Input.RegisterHotKey("vshud.markwaypoint", "(VSHUD) Mark Waypoint", GlKeys.BackSlash);
             api.Input.SetHotKeyHandler("vshud.markwaypoint", (a) =>
             {
-                MarkWPForBlockSel(api.World.Player.CurrentBlockSelection);
+                MarkWPForSelection(api.World.Player);
                 return true;
             });
+        }
+
+        public void MarkWPForSelection(IClientPlayer player)
+        {
+            if (player.CurrentBlockSelection != null)
+            {
+                MarkWPForBlockSel(player.CurrentBlockSelection);
+            }
+            else if (player.CurrentEntitySelection != null)
+            {
+                MarkWPForEntitySel(player.CurrentEntitySelection);
+            }
+        }
+
+        public void MarkWPForEntitySel(EntitySelection sel)
+        {
+            if (sel.Entity != null)
+            {
+                MarkWPForEntity(sel.Entity);
+            }
         }
 
         public void MarkWPForBlockSel(BlockSelection sel)
@@ -68,8 +105,24 @@ namespace VSHUD
             if (FloatyWaypointManagement.WaypointElements.Any(a => a.waypointPos.AsBlockPos.Equals(pos))) return;
 
             var midPos = pos.ToVec3d().Add(0.5, 0.5, 0.5);
-            var wp = WaypointClassifications[block.Code];
-            string str = string.Format(System.Globalization.CultureInfo.InvariantCulture, "/waypoint addati {0} ={1} ={2} ={3} {4} #{5} {6}", wp.Icon ?? "star1", midPos.X, midPos.Y, midPos.Z, false, wp.Color, wp.Name);
+            AssetLocation loc = GetClassifiedAssetLocation(block.Code, "block");
+
+            var wp = WaypointClassifications[loc];
+            MarkWP(wp, midPos);
+        }
+
+        public void MarkWPForEntity(Entity entity)
+        {
+            if (FloatyWaypointManagement.WaypointElements.Any(a => a.waypointPos.AsBlockPos.Equals(entity.Pos.AsBlockPos))) return;
+            AssetLocation loc = GetClassifiedAssetLocation(entity.Code, "item-creature");
+            
+            var wp = WaypointClassifications[loc];
+            MarkWP(wp, entity.Pos.XYZ);
+        }
+
+        public void MarkWP(WaypointClassification wp, Vec3d pos)
+        {
+            string str = string.Format(System.Globalization.CultureInfo.InvariantCulture, "/waypoint addati {0} ={1} ={2} ={3} {4} #{5} {6}", wp.Icon ?? "star1", pos.X, pos.Y, pos.Z, false, wp.Color, wp.Name);
 
             capi.SendChatMessage(str);
         }
