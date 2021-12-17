1b930ece417e4aaa2f2dec3e5e69a9f865df533f
Novocain <22160223+Novocain1@users.noreply.github.com>
2021-12-16 19:59:49 -0500

    set assembly version

--- VSHUD/VSHUDMain.cs
+++ VSHUD/VSHUDMain.cs
@@ -8,7 +8,7 @@ using Vintagestory.Client.NoObf;
     Description = "Automatically creates waypoints on player death, floaty waypoints, and other misc client side things",
     Side = "Client",
     Authors = new[] { "Novocain" },
-    Version = "2.0.15")]
+    Version = "2.0.17")]
 
 namespace VSHUD
 {
