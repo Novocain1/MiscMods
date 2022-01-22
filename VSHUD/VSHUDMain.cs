aae74e0014184991a9230624f9607ae5c847cff1
Novocain <22160223+Novocain1@users.noreply.github.com>
2022-01-22 06:36:29 -0500

    use correct version

--- VSHUD/VSHUDMain.cs
+++ VSHUD/VSHUDMain.cs
@@ -9,7 +9,7 @@ using Vintagestory.Client.NoObf;
     Side = "Client",
     Authors = new[] { "Novocain" },
     IconPath = "creative/textures/block/command01-inside.png",
-    Version = "2.0.28")]
+    Version = "2.0.29")]
 
 namespace VSHUD
 {
